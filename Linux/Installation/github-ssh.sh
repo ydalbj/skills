94e82860377c81dff0bbe37a55335c7079fa6a2e
liuhui <liuhuidut@sina.com>
2018-11-29 01:03:28 +0800

    修改：install.sh安装脚本新增zsh，f.lux安装命令

---
+++ Linux/Installation/github-ssh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# config github ssh access
+
--- Linux/Installation/install.sh
+++ Linux/Installation/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+name="liuhui"
+email="liuhuidut@sina.com"
+
+sudo apt update
+
 # install Vim
 echo "----------install Vim----------"
 sudo apt install vim -y
@@ -8,8 +13,8 @@ echo "----------Vim installed----------"
 # install Git
 echo "----------install Git----------"
 sudo apt install git -y
-git config --global user.name liuhui
-git config --global user.email liuhuidut@sina.com
+git config --global user.name ${name}
+git config --global user.email ${email}
 git config --global core.editor vim
 git config --global alias.st status
 git config --global alias.co checkout
@@ -19,3 +24,18 @@ git config --global alias.unstage "reset HEAD"
 git config --global alias.last "log -l"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 echo "----------Git installed----------"
+
+
+# install Zsh
+echo "----------install Git----------"
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "----------Git installed----------"
+
+# install f.lux
+echo "----------install f.lux----------"
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt update
+sudo apt install fluxgui -y
+echo "----------f.lux installed----------"
+
