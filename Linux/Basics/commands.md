39e602a94ddab738c6ee7c27bac3896743572fdd
liuhui <liuhuidut@sina.com>
2018-12-14 00:01:29 +0800

    add:elasticsearch安装脚本

--- Linux/Basics/commands.md
+++ Linux/Basics/commands.md
@@ -2,15 +2,21 @@
 
 
 ##### Shell命令
+* ps
+  - 判断系统SysV `init` or `systemd`
+    ```shell
+    $ ps -p -l
+    ```
+
 * `-` 减号 (dash)
     > 在 GNU 指令中，如果单独使用 - 符号，不加任何该加的文件名称时，代表\"标准输入\"的意思。这是 GNU指令的共通选项。譬如下例
     ```shell
-    tar xpvf -
+    $ tar xpvf -
     ```
     这里的 - 符号，既代表从标准输入读取资料。
 
 * `&` 后台工作
     > 单一个& 符号，且放在完整指令列的最后端，即表示将该指令列放入后台中工作。
     ```shell
-    tar cvfz data.tar.gz data > /dev/null&
+    $ tar cvfz data.tar.gz data > /dev/null&
     ```
---
+++ Linux/Installation/elasticsearch.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Elasticsearch 安装脚本
+# 安装不同版本请替换version变量
+
+version='6.5.2'
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${version}.deb.sha512
+shasum -a 512 -c elasticsearch-${version}.deb.sha512
+sudo dpkg -i elasticsearch-${version}.deb
+rm elasticsearch-${version}.deb elasticsearch-${version}.deb.sha512
+
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/rpm.html
+#SysV init vs systemdedit
+#Elasticsearch is not started automatically after installation. How to start and stop Elasticsearch depends on whether your system uses SysV init or systemd (used by newer distributions). You can tell which is being used by running this command:
+
+#ps -p 1
+#Running Elasticsearch with SysV initedit
+#Use the chkconfig command to configure Elasticsearch to start automatically when the system boots up:
+
+#sudo chkconfig --add elasticsearch
+#Elasticsearch can be started and stopped using the service command:
+
+#sudo -i service elasticsearch start
+#sudo -i service elasticsearch stop
+#If Elasticsearch fails to start for any reason, it will print the reason for failure to STDOUT. Log files can be found in /var/log/elasticsearch/.
+
+#Running Elasticsearch with systemdedit
+#To configure Elasticsearch to start automatically when the system boots up, run the following commands:
+
+#sudo /bin/systemctl daemon-reload
+#sudo /bin/systemctl enable elasticsearch.service
+#Elasticsearch can be started and stopped as follows:
+
+#sudo systemctl start elasticsearch.service
+#sudo systemctl stop elasticsearch.service
