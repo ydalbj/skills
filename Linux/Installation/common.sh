c11380c258088f16df94a2bb104ef05273f8246d
liuhui <liuhuidut@sina.com>
2019-01-05 16:11:40 +0800

    add:Linux common 安装脚本(gnome-tweak-tool,sudo nopasswd config, uget)

--- Linux/Installation/common.sh
+++ Linux/Installation/common.sh
@@ -10,6 +10,9 @@ if ! [ -x "$(command -v git)" ]; then
   # install Git
   echo "----------install Git----------"
   sudo apt install git -y
+  echo "----------Git installed----------"
+fi
+
   git config --global user.name ${name}
   git config --global user.email ${email}
   git config --global core.editor vim
@@ -21,8 +24,6 @@ if ! [ -x "$(command -v git)" ]; then
   git config --global alias.last "log -l"
   git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
   git config --global pull.rebase true
-  echo "----------Git installed----------"
-fi
 
 if ! [ -x "$(command -v zsh)" ]; then
   # install Zsh
@@ -56,10 +57,29 @@ if ! [ -x "$(command -v axel)" ]; then
   echo "----------composer installed----------"
 fi
 
+if ! [ -x "$(command -v gnome-tweak-tool)" ]; then
+  # install gnome-tweak-tool
+  echo "----------install gnome-tweak-tool----------"
+  sudo apt install gnome-tweak-tool
+  echo "----------install gnome-tweak-tool----------"
+fi
+
 if ! test -d ~/.vim_runtime
 then
   echo "----------install awesome vimrc----------"
   git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
   sh ~/.vim_runtime/install_awesome_vimrc.sh
   echo "----------awesome vimrc installed----------"
+fi
+
+# sudo nopassword
+if ! test -e "/etc/sudoers.d/mysudo"
+then
+  echo "%sudo  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/mysudo | cat > /dev/null
+fi
+
+if ! [ -x "$(command -v uget)"]; then
+  sudo add-apt-repository ppa:plushuang-tw/uget-stable
+  sudo apt update
+  sudo apt install uget aria2
 fi
