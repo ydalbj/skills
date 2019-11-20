c26f61d6cda3b86a8c12715c82a914e1f4cac7c6
liuhui <liuhuidut@sina.com>
2019-11-20 14:40:02 +0800

    update ssh_forward_monitor_tx script

--- Linux/Basics/ssh_forward_monitor_tx.sh
+++ Linux/Basics/ssh_forward_monitor_tx.sh
@@ -3,18 +3,19 @@
 host="tx.vps"
 user="ubuntu"
 
-while true ; do
-    sleep 10
-    NUM=`netstat -napt |grep ESTABLISHED |grep 140.143.224.225 |wc -l`
-    echo $NUM > /tmp/txvps
-    if [ "${NUM}" -lt "1" ]; then
-        echo "a"
-        ssh  -CqfTnN -R 17070:127.0.0.1:22 user@host  -p port
-    elif [ "${NUM}" -gt "1" ]; then
-        echo "b"
-        killall -9 ssh
-        ssh  -CqfTnN -R 17070:127.0.0.1:22 user@host  -p port
-    fi
+while true
+do
+  NUM=`netstat -napt |grep ESTABLISHED |grep 140.143.224.225 |wc -l`
+  echo $NUM > /tmp/txvps
+  if [ "${NUM}" -lt "1" ]; then
+    echo "a"
+    ssh -CqfTnN -R 2222:127.0.0.1:22 ${user}@${host}
+  elif [ "${NUM}" -gt "1" ]; then
+    echo "b"
+    killall -9 ssh
+    ssh -CqfTnN -R 2222:127.0.0.1:22 ${user}@${host}
+  fi
+  sleep 10
 done
 
 exit 0
